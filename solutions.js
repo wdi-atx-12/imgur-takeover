b6cc2cebedcf66a55d200ab15e9f543084ee73e9
Matt <mattray0295@gmail.com>
2017-09-21 18:09:02 -0500

    Matt Ray submitting 'imgur-takeover' homework

--- solutions.js
+++ solutions.js
@@ -1,5 +1,52 @@
 console.log('Defining functions!');
 
+
 function changeNavColor() {
   // change the navbar to a different color
-};
+  var navTop = document.querySelector('#topbar');
+  navTop.style.backgroundColor = 'green';
+}
+
+function addTopHeading() {
+  var createElement = document.createElement('h1');
+  createElement.setAttribute('class', 'customClass');
+  createElement.innerHTML = 'This page is now under Matt\'s control!';
+  document.getElementById('content').prepend(createElement);
+  createElement.style.fontSize = '50px';
+  createElement.style.color = 'blue';
+}
+
+function removeHalf() {
+  var links = [...document.querySelectorAll('.cards .image-list-link')];
+  var posts = [...document.querySelectorAll('.cards .post')];
+  var linksLength = links.length / 2;
+  for (let i = 0; i < linksLength; i++) {
+   links[i].parentNode.removeChild(links[i]);
+   posts[i].style.display = 'none';
+  }
+}
+
+function alertAfterImgClick() {
+  var selectImgs = [...document.querySelectorAll('.cards .image-list-link')];
+  
+  selectImgs.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      var getTagname = e.target.parentNode.tagName.toLowerCase();
+      var getAttr = e.target.parentNode.getAttribute('href');
+      
+      var promptUser = prompt("Type \'please\' to see this image.").toLowerCase();
+      if (promptUser === 'please') {
+        window.open(`http://imgur.com${getAttr}`, '_self');
+      }
+    });
+  });
+}
+
+changeNavColor();
+addTopHeading();
+removeHalf();
+alertAfterImgClick();
+
+
+
