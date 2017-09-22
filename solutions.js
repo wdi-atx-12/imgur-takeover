359ab8b5106dcfdce348bd0b5f9da6f6e9866fce
Peter Kong <bansuki@gmail.com>
2017-09-21 20:45:47 -0500

    nav bar color randomizer

--- solutions.js
+++ solutions.js
@@ -2,4 +2,16 @@ console.log('Defining functions!');
 
 function changeNavColor() {
   // change the navbar to a different color
+  document.querySelector('#topbar').style.backgroundColor = randomRGB();
 };
+
+//
+// helper functions
+//
+
+function randomRGB() {
+  let [r,g,b] = [256,256,256].map(n => {
+    return Math.floor(Math.random() * n);
+  });
+  return `rgb(${r},${g},${b})`;
+}
