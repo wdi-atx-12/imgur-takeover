921beb70cacfd1f3c1338fe454446e1427e8a1ea
Peter Kong <bansuki@gmail.com>
2017-09-21 21:16:55 -0500

    added header hijack function

--- solutions.js
+++ solutions.js
@@ -2,16 +2,35 @@ console.log('Defining functions!');
 
 function changeNavColor() {
   // change the navbar to a different color
-  document.querySelector('#topbar').style.backgroundColor = randomRGB();
-};
+  getTopbar().style.backgroundColor = randomRGB();
+}
+
+function hijackHeading() {
+  let heading = document.createElement('h1');
+  heading.innerText = 'This page is now under Peter Kong\'s control!!!';
+  heading.setAttribute('class', 'hijacked-heading');
+  heading.style.fontSize = '20px';
+  heading.style.padding = '0';
+  getTopbar().appendChild(heading);
+}
+
 
 //
 // helper functions
 //
 
+// returns random integer from 0 to n-1
+function rng(n) {
+  Math.floor(Math.random()*n);
+}
+
 function randomRGB() {
   let [r,g,b] = [256,256,256].map(n => {
-    return Math.floor(Math.random() * n);
+    return rng(n);
   });
   return `rgb(${r},${g},${b})`;
+}
+
+function getTopbar() {
+  return document.querySelector('#topbar');
 }
